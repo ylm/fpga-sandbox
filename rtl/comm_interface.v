700a0b6d70f452066fa5006faaeef2607827e583
Yannick Lamarre <yan.lamarre@gmail.com>
2021-04-15 20:52:18 -0400

    First draft of the communication protocol (start stop msg missing)

--- rtl/comm_interface.v
+++ rtl/comm_interface.v
@@ -22,8 +22,8 @@ reg init_done;
 reg [11:0] init_addr;
 reg [OUTPUT_WIDTH-1:0] init_data;
 reg [7:0] length;
-reg [7:0] rcv_bytes;
-reg [7:0] rcv_bytes_m1;
+reg [7:0] rcv_bytes_m1;	// Bytes rceived after length minus 1
+						// Effectively, the number of data bytes received
 reg [7:0] msgType;
 
 assign wr_enable = ~init_done;
@@ -55,33 +55,33 @@ uart #(
 // 0x05 = write RAM data
 always @(posedge clk) begin
 sys_reset <= 1'b0;
+wr_enable <= 1'b0;
 if (rx_intr) begin
 	if (|length) begin
-		if (rcv_bytes == 8'd0) begin
+		if (rcv_bytes_m1 == 8'hff) begin
 			msgType <= rx_data;
 		end else begin
-			case (msgType)
-				8'h00: ;
+			case (msgType) // See Communication Protocol above
+				8'h00: ; // RESERVED
 				8'h01:	sys_reset <= rx_data[0];
 				8'h02:	step <= {step[3:0], rx_data};
 				8'h03:	range <= {range[3:0], rx_data};
 				8'h04:	base_addr <= {base_addr[3:0], rx_data};
 				8'h05:	begin
-							sample_addr <= base_addr + rcv_bytes_m1[7:1];
-							sample_data <= {sample_data, rx_data};
-							wr_enable <= rcv_bytes_m1[0]
+							sample_addr <= base_addr + rcv_bytes_m1[7:1]; // Address increments every other bytes
+							sample_data <= {sample_data, rx_data}; // Sample data is 16bits
+							wr_enable <= rcv_bytes_m1[0]; // Toggles to one every other byte received
 						end
 				default: ;
 			endcase
 		end
-		rcv_bytes <= rcv_bytes + 8'd1;
+		rcv_bytes_m1 <= rcv_bytes_m1 + 8'd1;
 		length <= length - 8'd1;
 	end else begin
 		length <= rx_data;
 	end
 end
 if (reset) begin
-	rcv_bytes <= 8'd0;
 	rcv_bytes_m1 <= 8'hff;
 	length <= 8'd0;
 	msgType <= 8'd0;
